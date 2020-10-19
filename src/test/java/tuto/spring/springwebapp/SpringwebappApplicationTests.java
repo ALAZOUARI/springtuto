877a5cc0305e5d01756e22785272456d29c355fe
Ala <ala.zouariii@gmail.com>
2020-10-19 21:31:49 +0100

    added JPA

---
+++ src/test/java/tuto/spring/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package tuto.spring.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
